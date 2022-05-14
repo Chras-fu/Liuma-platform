a99441766eb8bfcf7180d5d668a35e62e8645cd3
chras-fu <chras_autotest@163.com>
2022-05-14 20:01:15 +0800

    新增接口测试前后置脚本功能

--- LiuMa-frontent/src/views/caseCenter/common/case/controller.vue
+++ LiuMa-frontent/src/views/caseCenter/common/case/controller.vue
@@ -13,7 +13,9 @@
             </el-table-column>
             <el-table-column label="值" prop="value">
                 <template slot-scope="scope">
-                    <el-input v-if="controller[scope.$index].name === 'sleepBeforeRun' | controller[scope.$index].name === 'sleepAfterRun'"
+                    <el-button v-if="controller[scope.$index].name === 'preScript' || controller[scope.$index].name === 'postScript'"
+                        size="mini" type="text" @click="editCode(scope.$index)">编辑脚本</el-button>
+                    <el-input v-else-if="controller[scope.$index].name === 'sleepBeforeRun' || controller[scope.$index].name === 'sleepAfterRun'"
                         size="small" style="width: 95%" placeholder="请输入等待时间 单位秒" v-model="controller[scope.$index].value"/>
                     <el-select v-else size="small" style="width: 95%" v-model="controller[scope.$index].value">
                         <el-option v-for="item in optList" :key="item.value" :label="item.label" :value="item.value"/>
@@ -28,13 +30,30 @@
         </el-table>
         <el-button size="small" icon="el-icon-plus" type="text" @click="add">新增</el-button>
         <el-button size="small" type="text" @click="deleteAll">删除全部</el-button>
-
+        <div v-if="showCode" style="margin-top: 20px">
+            <el-row>
+                <el-col :span="20">
+                    <p class="tip">
+                        <span>编辑脚本</span>
+                        <el-tooltip :content="text" placement="bottom" effect="light">
+                        <i class="el-icon-info"></i>
+                        </el-tooltip>
+                    </p>
+                </el-col>
+                <el-col :span="4">
+                    <el-button size="small" type="cancel" @click="showCode=false">取消</el-button>
+                    <el-button size="small" type="primary" @click="saveCode">保存</el-button>
+                </el-col>
+            </el-row>
+            <code-edit ref="editor" :data.sync='code' :height='480' mode="python"/>
+        </div>
     </div>
 </template>
 <script>
-
+import CodeEdit from '@/views/common/business/codeEdit'
 export default {
     name: 'Controller',
+    components: { CodeEdit },
     props:{
         controller: Array,
     },
@@ -47,11 +66,17 @@ export default {
                 {label: "保存Session", name: "saveSession"},
                 {label: "文件下载缓冲", name: "requireStream"},
                 {label: "证书验证", name: "requireVerify"},
+                {label: "前置脚本", name: "preScript"},
+                {label: "后置脚本", name: "postScript"},
             ],
             optList: [
                 {label: "是", value: true},
                 {label: "否", value: false},
-            ]
+            ],
+            showCode: false,
+            code: "",
+            index: null,
+            text: "获取关联参数/公共参数使用sys_get(name)函数，保存关联参数使用sys_put(name, val)函数，支持直接使用变量名res_code/res_header/res_data/res_cookies/res_bytes获取响应内容"
         }
     },
     methods: {
@@ -70,6 +95,15 @@ export default {
             }else{
                 this.controller[index].value = false;
             }
+        },
+        editCode(index){
+            this.index = index;
+            this.code = this.controller[index].value;
+            this.showCode = true;
+        },
+        saveCode(){
+            this.controller[this.index].value = this.code;
+            this.showCode = false;
         }
     }
 }
