01dd49442a7b45eaa124f76cef9dafadb6921414
Chras-fu <96771570+Chras-fu@users.noreply.github.com>
2022-12-13 17:04:30 +0800

    Update TestCaseAppResponse.java

--- LiuMa-backend/src/main/java/com/autotest/LiuMa/response/TestCaseAppResponse.java
+++ LiuMa-backend/src/main/java/com/autotest/LiuMa/response/TestCaseAppResponse.java
@@ -1,5 +1,6 @@
 package com.autotest.LiuMa.response;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,12 +12,18 @@ import java.util.List;
 public class TestCaseAppResponse extends TestCaseResponse{
 
     @JSONField(ordinal = 6)
-    private Boolean startDriver;
+    private String appId;
 
     @JSONField(ordinal = 7)
-    private Boolean closeDriver;
+    private String activity;
 
     @JSONField(ordinal = 8)
-    private List<TestCaseAppDataResponse> optList; // WEB用例的操作列表
+    private String deviceSystem;
+
+    @JSONField(ordinal = 9)
+    private String deviceUrl;
+
+    @JSONField(ordinal = 10)
+    private List<TestCaseAppDataResponse> optList; // APP用例的操作列表
 
 }
